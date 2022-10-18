ed831a3105d5854635f21db890dc6b30e5f9ddfb
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-10-18 12:14:43 +0200

    added loops

--- tuesday/session_examples_tuesday.py
+++ tuesday/session_examples_tuesday.py
@@ -1,3 +1,4 @@
+from random import randint
 # Recap Day 1
 
 name = "Anna"
@@ -28,11 +29,55 @@ elif age==16:
 else:
     print("TOO YOUNG!!")
 
-
-
-
-
 # Loops
+
+# while
+# unknown ammount of loops
+
+its_raining = True
+
+while its_raining:
+    print("Rain!")
+    num = randint(1,100)
+    if num==50:
+        its_raining=False
+
+seconds = 0
+
+while seconds<60:
+    print("Not a minute")
+    print(seconds)
+    if seconds==30:
+        print("Halftime")
+        seconds += 10
+        continue
+    if seconds==45:
+        break
+    seconds += 1
+print("minute!")
+
+
+# for
+# known ammounts of loops
+
+text = "Hello World"
+
+for currywurst in text:
+    print(currywurst)
+
+
+for number in range(11):
+    print(number)
+
+print('++++++++')
+
+for number in range(5,11):
+    print(number)
+
+print('+++++++')
+
+for whatever in range(2,27,2):
+    print(whatever)
 
 
 # Function
