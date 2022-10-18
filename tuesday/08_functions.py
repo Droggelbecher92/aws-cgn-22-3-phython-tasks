641cc9a1ce81c4bfd4212dc16784d8cd86eee8f3
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-10-18 09:07:38 +0200

    new task

---
+++ tuesday/08_functions.py
@@ -0,0 +1,14 @@
+# write a function that prints out if the the division of two numbers has a remainder
+
+def has_remainder():
+    print()
+#write a function that returns a given string in reverse order
+
+def encrypt():
+    return
+
+#write a function that takes a string and returns the snakecase version of this string
+# Example: This is a file => this_is_a_file
+
+def snakecaser():
+    return
